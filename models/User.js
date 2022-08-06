fc1bf1a4246171f6c93d8cd1ffc9eed925b654b9
Eddiem-11 <emonarrez.01@outllok.com>
2022-08-06 12:03:11 -0600

    added to users.js and seeds

--- models/User.js
+++ models/User.js
@@ -28,6 +28,14 @@ User.init(
         isUser_name: true,
       },
     },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+      validate: {
+        isEmail: true,
+      },
+    },
     password: {
       type: DataTypes.STRING,
       allowNull: false,
