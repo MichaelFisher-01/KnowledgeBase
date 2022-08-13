1c0d1f9194b23c3a465260b1fb7792faaca506c3
Michael Fisher <fisherdesign3@gmail.com>
2022-08-13 09:36:55 -0600

    Updates to post routes

--- controller/api/index.js
+++ controller/api/index.js
@@ -1,6 +1,8 @@
 const router = require('express').Router();
 const userRoutes = require('./userRoutes');
+const postRouter = require('./postRoutes');
 
 router.use('/users', userRoutes);
+router.use('/newPost', postRouter);
 
 module.exports = router;
